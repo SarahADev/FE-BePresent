eea2c88852a4fd6f8ce0f4e44bc597e58a4acaa1
ostroumova <g.ostroumova@gmail.com>
2022-09-13 17:07:57 +0100

    move buttons

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,99 +1,107 @@
 <template>
   <section>
-  <form @submit="checkUser">
-    <h2>Login</h2>
-    <label for="">Email:</label>
-    <input type="email" required v-model="email" />
+    <form @submit="checkUser">
+      <h2>Login</h2>
+      <label for="">Email:</label>
+      <input type="email" required v-model="email" />
 
-    <label>Password:</label>
-    <input type="password"  minlength="6" maxlength="30" required v-model="password" />
+      <label>Password:</label>
+      <input
+        type="password"
+        minlength="6"
+        maxlength="30"
+        required
+        v-model="password"
+      />
+      <div class="center">
+        <button>Login</button>
+      </div>
+    </form>
 
-    <button>Login</button>
-
-  </form>
-
-  <p class="or">or</p>
-
-  <button @click="$router.push('signup')">Create Account</button>
-  <p v-if="err">That email or password is incorrect, please try again.</p>
- 
-</section>
+    <p class="or">or</p>
+    <div class="center">
+      <button @click="$router.push('signup')">Create Account</button>
+      <p v-if="err">That email or password is incorrect, please try again.</p>
+    </div>
+  </section>
 </template>
 
 <script>
-  import axios from 'axios'
-  export default {
-    data() {
-      return {
-        email: "",
-        password: "",
-        err: false,
-        userId: "",
-      };
-    },
-    methods: {
-      async checkUser() {
-        await axios.post("https://be-present.fly.dev/users/login", {
+import axios from "axios";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      err: false,
+      userId: "",
+    };
+  },
+  methods: {
+    async checkUser() {
+      await axios
+        .post("https://be-present.fly.dev/users/login", {
           email: this.email,
-          password: this.password
+          password: this.password,
         })
-        .then(({data}) => {
-          this.userId = data.user_id
-          this.$router.push({name: `calendar`, params: {userId: this.userId}})
+        .then(({ data }) => {
+          this.userId = data.user_id;
+          this.$router.push({
+            name: `calendar`,
+            params: { userId: this.userId },
+          });
         })
         .catch(() => {
           this.err = true;
-        })
-      },
+        });
     },
-  }
-
+  },
+};
 </script>
 
-
-
 <style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: #f6f7f8;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label {
+  color: #ea9010;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
 
-  form {
-    max-width: 420px;
-    margin: 30px auto;
-    background: #f6f7f8;
-    text-align: left;
-    padding: 40px;
-    border-radius: 10px;
-  }
-  label {
-    color: #ea9010;
-    display: inline-block;
-    margin: 25px 0 15px;
-    font-size: 0.6em;
-    text-transform: uppercase;
-    letter-spacing: 1px;
-    font-weight: bold;
-  }
-
-  button {
-    background: #4e937a;
-    border: 0;
-    padding: 10px 20px;
-    margin-top: 20px;
-    color: white;
-    border-radius: 20px;
-  }
-  .submit {
-    text-align: center;
-  }
-  .error {
-    color: red;
-    margin-top: 10px;
-    font-weight: bold;
-    font-size: 0.8e;
-  }
-  .or {
-    color: rgb(184, 184, 184);
-    font-size: 0.6em;
-    text-transform: uppercase;
-    letter-spacing: 2px;
-    font-weight: bold;
-  }
+button {
+  background: #4e937a;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 0;
+  color: white;
+  border-radius: 20px;
+}
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.error {
+  color: red;
+  margin-top: 10px;
+  font-weight: bold;
+  font-size: 0.8e;
+}
+.or {
+  color: rgb(184, 184, 184);
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-weight: bold;
+}
 </style>
--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,196 +1,219 @@
 <template>
-    <section>
+  <section>
     <form @submit="handleSubmit">
-        <h2>Create Account</h2>
+      <h2>Create Account</h2>
 
-        <label for="">First Name</label>
-        <input type="name" required v-model="firstName">
+      <label for="">First Name</label>
+      <input type="name" required v-model="firstName" />
 
-        <label for="">Last Name</label>
-        <input type="name" required v-model="lastName">
+      <label for="">Last Name</label>
+      <input type="name" required v-model="lastName" />
 
-        <label for="">Email:</label>
-        <input type="email" required v-model="email">
+      <label for="">Email:</label>
+      <input type="email" required v-model="email" />
 
-        <label>Password:</label>
-        <input type="password" minlength="6" maxlength="30" required v-model="password">
-        <!-- <div v-if="passwordError" class="error">{{ passwordError }}</div> -->
+      <label>Password:</label>
+      <input
+        type="password"
+        minlength="6"
+        maxlength="30"
+        required
+        v-model="password"
+      />
+      <!-- <div v-if="passwordError" class="error">{{ passwordError }}</div> -->
 
-        <label for="">Date of Birth:</label>
-        <div class="dob">
-        <input type="text" minlength="2" maxlength="2" v-model="day">
+      <label for="">Date of Birth:</label>
+      <div class="dob">
+        <input type="text" minlength="2" maxlength="2" v-model="day" />
         <p>/</p>
-        <input type="text" minlength="2" maxlength="2" v-model="month">
+        <input type="text" minlength="2" maxlength="2" v-model="month" />
         <p>/</p>
-        <input type="text" minlength="4" maxlength="4" v-model="year">
+        <input type="text" minlength="4" maxlength="4" v-model="year" />
         <!-- <div v-if="yearError" class="error">{{ yearError }}</div> -->
-    
+
         <!-- <input type="date" required v-model="dob"> -->
-        </div>
-        <p class="date-eg">format: day/month/year</p>
-        <p class="date-eg">example: 01/01/1990</p>
+      </div>
+      <p class="date-eg">format: day/month/year</p>
+      <p class="date-eg">example: 01/01/1990</p>
 
-        <label>Interests:</label>
-        <div class="interest-list" required>
-            <ul>
-                <input type="checkbox" value="art-and-collectibles" v-model="interests">
-                <label>Art & Collectibles</label>
-                <input type="checkbox" value="jewelry-and-accessories" v-model="interests">
-                <label>Jewelry & Accessories</label>
-                <input type="checkbox" value="home-and-living" v-model="interests">
-                <label>Home & Living</label>
-                <input type="checkbox" value="clothing-and-shoes" v-model="interests">
-                <label>Clothing & Shoes</label>
-                <input type="checkbox" value="toys-and-entertainment" v-model="interests">
-                <label>Toys & Entertainment</label>
-            </ul>
-            <!-- 'jewelry-and-accessories', 'clothing-and-shoes', 'home-and-living', 'wedding-and-party', 'toys-and-entertainment', 'art-and-collectibles' -->
-            
-        </div>
+      <label>Interests:</label>
+      <div class="interest-list" required>
+        <ul>
+          <input
+            type="checkbox"
+            value="art-and-collectibles"
+            v-model="interests"
+          />
+          <label>Art & Collectibles</label>
+          <input
+            type="checkbox"
+            value="jewelry-and-accessories"
+            v-model="interests"
+          />
+          <label>Jewelry & Accessories</label>
+          <input type="checkbox" value="home-and-living" v-model="interests" />
+          <label>Home & Living</label>
+          <input
+            type="checkbox"
+            value="clothing-and-shoes"
+            v-model="interests"
+          />
+          <label>Clothing & Shoes</label>
+          <input
+            type="checkbox"
+            value="toys-and-entertainment"
+            v-model="interests"
+          />
+          <label>Toys & Entertainment</label>
+        </ul>
+        <!-- 'jewelry-and-accessories', 'clothing-and-shoes', 'home-and-living', 'wedding-and-party', 'toys-and-entertainment', 'art-and-collectibles' -->
+      </div>
 
-        <br>
+      <br />
 
-        <div class="terms">
-            <input type="checkbox" v-model="terms" required>
-            <label>Accept Terms and Conditions</label>
-        </div>
+      <div class="terms">
+        <input type="checkbox" v-model="terms" required />
+        <label>Accept Terms and Conditions</label>
+      </div>
 
-        <div class="terms">
-            <input type="checkbox" v-model="reminders">
-            <label>Recieve email reminders for your upcoming events</label>
-        </div>
+      <div class="terms">
+        <input type="checkbox" v-model="reminders" />
+        <label>Recieve email reminders for your upcoming events</label>
+      </div>
 
-        <button class="submit">Create Account</button>
-    
-        
+      <div class="center">
+        <button>Create Account</button>
+      </div>
     </form>
+
     <p class="or">or</p>
-    <button @click="$router.push('/')">Already have an account?</button>
-    </section>
-    <!-- <p>first name: {{ firstName }}</p>
+    <div class="center">
+      <button @click="$router.push('/')">Already have an account?</button>
+    </div>
+  </section>
+  <!-- <p>first name: {{ firstName }}</p>
     <p>last name: {{ lastName }}</p>
     <p>Email: {{ email }}</p>
     <p>Password: {{ password }}</p> -->
-    <!-- <p>PasswordError: {{passwordError}}</p>
+  <!-- <p>PasswordError: {{passwordError}}</p>
     <p>yearError: {{yearError}}</p> -->
-    <!-- <p>Date of Birth: {{day}} / {{month}} / {{year}}</p>
+  <!-- <p>Date of Birth: {{day}} / {{month}} / {{year}}</p>
     <p>Terms Accepted: {{terms}}</p>
     <p>Preferences: {{interests}}</p> -->
 </template>
 
 <script>
-import axios from 'axios' 
-    export default {
-        data() {
-            return {
-                firstName: '',
-                lastName: '',
-                email: '',
-                password: '',
-                day: '',
-                month: '',
-                year: '',
-                reminders: true,
-                terms: false,
-                interests:[],
-                passwordError: '',
-                // yearError: ''
-            }
-        },
-        methods: {
-            
-            async addUser() {
-               
-                await axios.post("https://be-present.fly.dev/users", {
-                    first_name:this.firstName,
-                    last_name:this.lastName,
-                    email:this.email,
-                    birth_day:this.day,
-                    birth_month:this.month,
-                    birth_year:this.year,
-                    password:this.password,
-                    interests:this.interests
-
-                })
-                .catch((error) => {
-                    console.log(error)
-                })
-            },
-            handleSubmit() {
-            this.addUser()
-            this.$router.push('/');
-            },
-        }
-    }
+import axios from "axios";
+export default {
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      day: "",
+      month: "",
+      year: "",
+      reminders: true,
+      terms: false,
+      interests: [],
+      passwordError: "",
+      // yearError: ''
+    };
+  },
+  methods: {
+    async addUser() {
+      await axios
+        .post("https://be-present.fly.dev/users", {
+          first_name: this.firstName,
+          last_name: this.lastName,
+          email: this.email,
+          birth_day: this.day,
+          birth_month: this.month,
+          birth_year: this.year,
+          password: this.password,
+          interests: this.interests,
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    handleSubmit() {
+      this.addUser();
+      this.$router.push("/");
+    },
+  },
+};
 </script>
 
 <style>
-    form {
-        max-width: 420px;
-        margin: 30px auto;
-        background: #F6F7F8;
-        text-align: left;
-        padding: 40px;
-        border-radius: 10px;
-    }
-    label {
-        color: #ea9010;
-        display: inline-block;
-        margin: 25px 0 15px;
-        font-size: 0.6em;
-        text-transform: uppercase;
-        letter-spacing: 1px;
-        font-weight: bold;
-    }
-    input, select{
-        display:block;
-        padding: 10px 6px;
-        width: 100%;
-        box-sizing: border-box;
-        border: none;
-        border-bottom: 1px solid #ef767a;
-        color: #555
-    }
-    input[type="checkbox"] {
-        display:inline-block;
-        width: 16px;
-        margin: 0 10px 0 0;
-        position: relative;
-        top: 2px;
-    }
-    label.terms {
-        color:#555;
-    }
-    button {
-        background: #4e937a;
-        border: 0;
-        padding: 10px 20px;
-        margin-top: 20px;
-        color: white;
-        border-radius: 20px;
-    }
-    .submit {
-        text-align: center;
-    }
-    .error {
-        color:red;
-        font-size: 0.6em;
-        text-transform: uppercase;
-        letter-spacing: 1px;
-        font-weight: bold;
-    }
-    .dob {
-        display: flex;
-        align-content: center;
-
-
-    }
-    .date-eg, .or {
-         color: rgb(184, 184, 184);
-        font-size: 0.6em;
-        text-transform: uppercase;
-        letter-spacing: 2px;
-        font-weight: bold;
-    }
-
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: #f6f7f8;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label {
+  color: #ea9010;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input,
+select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ef767a;
+  color: #555;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+label.terms {
+  color: #555;
+}
+button {
+  background: #4e937a;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 0;
+  color: white;
+  border-radius: 20px;
+}
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.error {
+  color: red;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+.dob {
+  display: flex;
+  align-content: center;
+}
+.date-eg,
+.or {
+  color: rgb(184, 184, 184);
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-weight: bold;
+}
 </style>
