82eca7688dd0ff40e24541669edbf81634c2df28
RichardNeat <rneat@hotmail.co.uk>
2022-09-14 09:53:21 +0100

    navBar completed

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -1,38 +1,23 @@
 <template>
 <Header/>
-    
         <div class="calendar">
             <router-view name="calendar"></router-view>
         </div>
-        <nav class="calendar-nav">
-            <button @click="handleClickGetPresents">Get Presents!</button>
-            <!-- <button @click="handleClickHome">Home</button> -->
-            <button @click="handleClickProfile">My Profile</button>
-        </nav>
-   
-       
+        <Navbar/>
         <CalendarProfiles/>
         <Calendar/>
-        
 </template>
 
 <script>
 import Calendar from '@/components/Calendar.vue';
 import CalendarProfiles from '@/components/CalendarProfiles.vue';
 import Header from '@/components/Header.vue';
+import Navbar from '@/components/Navbar.vue';
     export default {
     data() {
         return {};
     },
-    methods: {
-        async handleClickProfile() {
-            await this.$router.push({ name: `user-profile`, params: { userId: this.$route.params.userId } });
-        },
-        async handleClickHome() {
-            await this.$router.push({ name: `calendar`, params: { userId: this.$route.params.userId } });
-        }
-    },
-    components: { Calendar, CalendarProfiles, Header }
+    components: { Calendar, CalendarProfiles, Header, Navbar }
 };
 </script>
 
